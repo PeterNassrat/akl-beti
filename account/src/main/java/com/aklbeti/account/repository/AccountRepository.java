931ca6f082bb1e5c4c88b39064528904deabacde
Peter Nassrat <peternassrat90@gmail.com>
2025-04-05 07:23:21 +0200

    Edit the AccountRepository.java file due to adding account ID of type long to the account entity

--- account/src/main/java/com/aklbeti/account/repository/AccountRepository.java
+++ account/src/main/java/com/aklbeti/account/repository/AccountRepository.java
@@ -4,7 +4,10 @@ import com.aklbeti.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface AccountRepository extends JpaRepository<Account, String> {
+import java.util.Optional;
 
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByEmail(String emailAddress);
 }
