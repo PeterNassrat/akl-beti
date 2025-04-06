9e34cdbf348ba935f85f092b771c2ec85db86858
Peter Nassrat <peternassrat90@gmail.com>
2025-04-06 10:38:50 +0200

    Rename the AccountException.java to AccountCreationException.java

---
+++ account/src/main/java/com/aklbeti/account/exception/AccountCreationException.java
@@ -0,0 +1,8 @@
+package com.aklbeti.account.exception;
+
+public class AccountCreationException extends RuntimeException {
+
+    public AccountCreationException(String msg) {
+        super(msg);
+    }
+}
--- account/src/main/java/com/aklbeti/account/exception/AccountException.java
+++
@@ -1,8 +0,0 @@
-package com.aklbeti.account.exception;
-
-public class AccountException extends RuntimeException {
-
-    public AccountException(String msg) {
-        super(msg);
-    }
-}
