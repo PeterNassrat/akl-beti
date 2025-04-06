6b197103ab07bb1ba05a98faf284e19cfa6a9177
Peter Nassrat <peternassrat90@gmail.com>
2025-04-06 10:27:03 +0200

    Rename and update the AccountRequest.java dto to RegistrationRequest.java file

--- account/src/main/java/com/aklbeti/account/dto/RegistrationRequest.java
+++ account/src/main/java/com/aklbeti/account/dto/RegistrationRequest.java
@@ -1,21 +1,16 @@
 package com.aklbeti.account.dto;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.*;
 
-public record AccountRequest(
-
-        @PositiveOrZero(message = "ID must be positive or zero")
-        long id,
+public record RegistrationRequest(
 
         @NotBlank(message = "Email address is required!")
         @Email(message = "Email address is not valid!")
         String emailAddress,
 
         @NotBlank(message = "Password is required!")
+        @Size(min = 8, message = "Password must be at least 8 characters!")
         String password,
 
         @Valid
