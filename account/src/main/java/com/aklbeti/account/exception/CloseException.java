31f3a9e421abccc7a8c64d54f929c6493e9b6374
Peter Nassrat <peternassrat90@gmail.com>
2025-04-07 11:09:48 +0200

    Create exception files

---
+++ account/src/main/java/com/aklbeti/account/exception/CloseException.java
@@ -0,0 +1,7 @@
+package com.aklbeti.account.exception;
+
+public class CloseException extends RuntimeException {
+    public CloseException(String message) {
+        super(message);
+    }
+}
---
+++ account/src/main/java/com/aklbeti/account/exception/UpdateException.java
@@ -0,0 +1,7 @@
+package com.aklbeti.account.exception;
+
+public class UpdateException extends RuntimeException {
+    public UpdateException(String message) {
+        super(message);
+    }
+}
