27147fb5e511f6f489414af2044c64a7959fac1c
Peter Nassrat <peternassrat90@gmail.com>
2025-04-09 23:40:16 +0200

    Refactor imports

--- account/src/main/java/com/aklbeti/account/dto/AddressRequest.java
+++ account/src/main/java/com/aklbeti/account/dto/AddressRequest.java
@@ -1,10 +1,7 @@
 package com.aklbeti.account.dto;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record AddressRequest(
 
